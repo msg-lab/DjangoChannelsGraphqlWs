e37937ea5723b2cf71fdd40925c3abcec5a648bc
Andrey Zelenchuk <azelenchuk@plesk.com>
2020-03-19 21:57:03 +0700

    Introduce initial payload.

--- channels_graphql_ws/graphql_ws_consumer.py
+++ channels_graphql_ws/graphql_ws_consumer.py
@@ -679,6 +679,7 @@ class GraphqlWsConsumer(ch_websocket.AsyncJsonWebsocketConsumer):
         groups,
         publish_callback,
         unsubscribed_callback,
+        initial_payload,
         notification_queue_limit=None,
     ):
         """Register a new subscription when client subscribes.
@@ -697,6 +698,7 @@ class GraphqlWsConsumer(ch_websocket.AsyncJsonWebsocketConsumer):
                 subscription groups current subscription belongs to.
             unsubscribed_callback: Called to notify when a client
                 unsubscribes.
+            initial_payload: Initial payload.
             notification_queue_limit: LImit for the subscribtion
                 notification queue. Default is used if not set.
 
@@ -720,6 +722,10 @@ class GraphqlWsConsumer(ch_websocket.AsyncJsonWebsocketConsumer):
             queue_size = self.subscription_notification_queue_limit
         notification_queue = asyncio.Queue(maxsize=queue_size)
 
+        # Enqueue the initial payload.
+        if initial_payload is not self.SKIP:
+            notification_queue.put_nowait(Serializer.serialize(initial_payload))
+
         # Start an endless task which listens the `notification_queue`
         # and invokes subscription "resolver" on new notifications.
         async def notifier():
@@ -727,6 +733,14 @@ class GraphqlWsConsumer(ch_websocket.AsyncJsonWebsocketConsumer):
 
             # Assert we run in a proper thread.
             self._assert_thread()
+
+            # Dirty hack to partially workaround the race between:
+            #  1) call to `result.subscribe` in `_on_gql_start`; and
+            #  2) call to `trigger.on_next` below in this function.
+            # The first call must be earlier. Otherwise, first one or more notifications
+            # may be lost.
+            await asyncio.sleep(1)
+
             while True:
                 serialized_payload = await notification_queue.get()
 
